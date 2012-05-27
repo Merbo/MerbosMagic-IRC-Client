531f320f4de46815feaf915350f393133cfdf497
Merbo <carrethan02@gmail.com>
2012-05-27 19:46:10 -0400

    Added processing of PRIVMSGs for channels to the GUI. Nothing else is sent to anything but the debugPage tab. Also, debugPage is only available when the project is compiled in debug configuration.

--- MerbosMagic IRC Client/IRC.cs
+++ MerbosMagic IRC Client/IRC.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace MerbosMagic_IRC_Client
 {
@@ -40,7 +41,6 @@ namespace MerbosMagic_IRC_Client
 
             ReadInput();
         }
-        //:ClientTest!ClientTest@108.49.53.188 JOIN :#MerbosMagic
 
         public static void ProcessRaw(string raw) {
 #if DEBUG
@@ -61,6 +61,18 @@ namespace MerbosMagic_IRC_Client
                             Program.M.AddPage(commands[2].Remove(0, 2), commands[2].Remove(0, 1));
                         }
                         break;
+                    case "PRIVMSG":
+                        string whattheysaid = String.Join(" ", commands, 3, commands.Length - 3).Remove(0, 1);
+                        string whotheyare = commands[0].Remove(0, 1);
+                        string nicksep = "!";
+                        int i = whotheyare.IndexOf(nicksep);
+                        if (i >= 0)
+                        {
+                            whotheyare = whotheyare.Remove(i, whotheyare.Length - i);
+                        }
+                        string text = "<" + whotheyare + "> " + whattheysaid;
+                        Program.M.ChatAdd(commands[2].Remove(0, 1), text);
+                        break;
                 }
             }
         }
@@ -78,7 +90,7 @@ namespace MerbosMagic_IRC_Client
                     if (input != null)
                     {
                         ProcessRaw(input);
-                        Thread.Sleep(50);
+                        //Thread.Sleep(25);
                     }
                 }
                 catch (NullReferenceException)
@@ -93,6 +105,13 @@ namespace MerbosMagic_IRC_Client
 #if DEBUG
             Program.M.ChatAdd("debugPage", "--> " + raw);
 #endif
+            string[] commands = raw.Split(' ');
+
+            if (commands[0] == "PRIVMSG")
+            {
+                Program.M.ChatAdd(commands[1].Remove(0, 1), "<" + nick + "> " + String.Join(" ", commands, 2, commands.Length - 2).Remove(0, 1));
+            }
+
             if (IRCClient.Connected)
             {
                 IRCWriter.WriteLine(raw);
