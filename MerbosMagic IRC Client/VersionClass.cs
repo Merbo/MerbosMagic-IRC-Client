76db225385c01fbc9c14a74b14406cc72abe2a96
Merbo <carrethan02@gmail.com>
2012-06-14 17:26:21 -0400

    VersionClass now can return an int[] if necessary for processing in-client.

--- MerbosMagic IRC Client/VersionClass.cs
+++ MerbosMagic IRC Client/VersionClass.cs
@@ -10,12 +10,16 @@ namespace MerbosMagic_IRC_Client
         private int _MajorVersion;
         private int _MinorVersion;
         private int _SubVersion;
+
+
+
         public VersionClass(int MajorVersion, int MinorVersion, int Subversion)
         {
             _MajorVersion = MajorVersion;
             _MinorVersion = MinorVersion;
             _SubVersion = Subversion;
         }
+
         public string Text
         {
             get
@@ -23,8 +27,22 @@ namespace MerbosMagic_IRC_Client
                 return _MajorVersion.ToString() +"."+ _MinorVersion.ToString() +"."+ _SubVersion.ToString();
             }
         }
-        public static implicit operator string(VersionClass c){
+
+        public static implicit operator string(VersionClass c) //This is for referring to it as a string.
+        {
             return c._MajorVersion.ToString() + "." + c._MinorVersion.ToString() + "." + c._SubVersion.ToString();
+        }
+
+        public static implicit operator int[](VersionClass c) //We can get an int[] if we're gonna be handling the version inside the client.
+        {
+            int[] i = 
+            {
+                c._MajorVersion,
+                c._MinorVersion,
+                c._SubVersion
+            };
+
+            return i;
         }
             
     }
