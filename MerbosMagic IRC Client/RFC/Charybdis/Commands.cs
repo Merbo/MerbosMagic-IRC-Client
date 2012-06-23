4377850f9b39c4a578168b8e7554bd9e5f0196f2
Merbo <carrethan02@gmail.com>
2012-06-23 11:42:20 -0400

    Add definitions for all IRCds I could find, as well as services packages.

---
+++ MerbosMagic IRC Client/RFC/Charybdis/Commands.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MerbosMagic_IRC_Client.RFC
+{
+    class RFC_Charybdis_Commands : RFC
+    {
+    }
+}
