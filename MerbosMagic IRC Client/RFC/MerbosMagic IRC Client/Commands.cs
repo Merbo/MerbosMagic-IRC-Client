bb06c8f4bb5b7f3d489cddd0a4fa3b734514647b
Merbo <carrethan02@gmail.com>
2012-06-23 11:48:19 -0400

    Herp, added client-specific commands. We'll need this for /server etc.

---
+++ MerbosMagic IRC Client/RFC/MerbosMagic IRC Client/Commands.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MerbosMagic_IRC_Client.RFC
+{
+    class RFC_MerbosMagic_IRC_Client_Commands : RFC
+    {
+    }
+}
